16f2fbffb78e843b5b58996022d68d8a482fb7dd
Nick Villone <n.villone8@gmail.com>
2022-08-28 19:58:43 -0400

    create a model class

--- main/java/org/launchcode/codingevents/controllers/EventController.java
+++ main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,25 +15,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayedEvents(Model model){
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
 
-    //lives at /events/create
     @GetMapping("create")
-    public String displayCreatedEventsForm(){
+    public String displayCreatedEventsForm(Model model){
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //lives at /events/create
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:";  //instructs browser to redirect to the root path
     }
 
