426a08575a4a1a2af57e8508e16bc00aabf14f01
Nick Villone <n.villone8@gmail.com>
2022-08-28 20:34:20 -0400

    add a model property

--- main/java/org/launchcode/codingevents/models/Event.java
+++ main/java/org/launchcode/codingevents/models/Event.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class Event {
 
-    private  String name;
+    private String name;
+    private String description;
 
-    public Event(String name) {
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
@@ -17,6 +19,14 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
