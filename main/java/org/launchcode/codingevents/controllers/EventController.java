c53541067573e02e0793aa6d1819c2da5c1c1e5a
Nick Villone <n.villone8@gmail.com>
2022-09-21 21:12:51 -0400

    updated EventController by adding a variable EventRepository, created EventRepository interface, and updated Event class to use eventRepository variable

--- main/java/org/launchcode/codingevents/controllers/EventController.java
+++ main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,26 +1,29 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    //use findAll, save, findById methods from eventRepository
+
     @GetMapping
     public String displayedEvents(Model model) {;
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -41,14 +44,14 @@ public class EventController {
             model.addAttribute("errorMsg", "Bad data!");
             return "events/create";
         }
-        EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect:";  //instructs browser to redirect to the root path
     }
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
@@ -57,7 +60,7 @@ public class EventController {
 
         if(eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
 
--- main/java/org/launchcode/codingevents/data/EventRepository.java
+++ main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
-
-
 }
