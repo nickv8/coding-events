aab99a5002bf3a3520d49c5387e36f6321c953d3
Nick Villone <n.villone8@gmail.com>
2022-10-02 21:41:36 -0400

    updates

--- main/java/org/launchcode/codingevents/models/User.java
+++ main/java/org/launchcode/codingevents/models/User.java
@@ -13,11 +13,14 @@ public class User extends AbstractEntity {
     private String pwHash;
 
     public User() {}
+    
 
     public User(String username, String password) {
         this.username = username;
         this.pwHash = password;
     }
+
+
 
     public String getUsername() {
         return username;
